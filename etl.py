d44b99516d95647a88c8a2cf9d926108d441820d
Sam Ezebunandu <samuel.ezebunandu@outlook.com>
2020-06-21 09:54:54 -0600

    Docstrings to etl functions

--- etl.py
+++ etl.py
@@ -4,24 +4,32 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """Loads data into staging tables in Redshift."""
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """Insert data into the normalized tables."""
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """ETL process to load data into staging tables
+    and then move into final normalized tables."""
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read("dwh.cfg")
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(
+            *config["CLUSTER"].values()
+        )
+    )
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
